8b6bfff02797bc19c0f121b83aeee6f1ec7f0103
Jmelgar1014 <86273515+Jmelgar1014@users.noreply.github.com>
2021-08-28 23:29:21 -0400

    JS not needed yet

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const toggleBtn = document.getElementsByClassName('toggle-button');
-const navbarLinks = document.getElementsByClassName('nav-bar');
-
-
-toggleBtn.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
